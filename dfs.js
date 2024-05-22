ca0f43809f1c048371096b05ad7aba20ca665031
vladivanovpm <vladivanovpm@gmail.com>
2024-05-22 15:24:06 +0300

    Добавлен подсчет пути

--- dfs.js
+++ dfs.js
@@ -1,11 +1,15 @@
 function dfs(graph, startNode) {
     let visited = [];
-  
+    let pathLength = 0;
 
     
     function dfsUtil(node, count) {
         visited.push(node);
     
+        if (node === endNode) {
+            pathLength = count;
+        }
+        
         let neighbors = graph[node];
         for (let i = 0; i < neighbors.length; i++) {
             if (!visited.includes(neighbors[i])) {
@@ -14,14 +18,16 @@ function dfs(graph, startNode) {
         }
     }
     
-    dfsUtil(startNode);
-
+    dfsUtil(startNode, 0);
+    return pathLength_;
+    
     
     // Пример графа
     let graph = {
     A: ['B', 'C'],
-    B: ['A', 'C', 'E'],
+    B: ['A', 'C'],
     C: ['A', 'B'],
     }; 
 }
+    let pathLength = dfs(graph, 'A', 'C');
     console.log("Length of path from A to C: " + pathLength);
